83f0a832c5e1048a3087878c10654f9f60615582
chyoo <chyoo1991@gmail.com>
2016-12-14 16:52:28 +0800

    have opt

--- doMinitor.py
+++ doMinitor.py
@@ -1,38 +1,61 @@
 #!/usr/bin/python
 # coding:utf-8
-import login163
+import os
+import email163
 import requests
 from bs4 import BeautifulSoup
 
-url="http://www.whut.edu.cn/2015web/tzgg/"
-response = requests.get(url).text
 
-soup = BeautifulSoup(response,"html5lib")
-ul = soup.find_all('ul', attrs={"class":"normal_list2"})
-lis = ul[0].find_all("li")
-li_tag = lis[0]
+st_email = ["951004642@qq.com"]
+whut_url="http://www.whut.edu.cn/2015web/tzgg/"
+file_name=".st.log"
+file_path="."
+full_file=file_path + os.path.sep + file_name
 
-file_path="/home/chyoo/learning/shaotao/.shaotao"
-list_file = open(file_path)
-items = list_file.readlines()
 
-if len(items) == 0:
-    for item in lis:
-        list_file.write(item.find("a").get("href") + "\t" + item.find("strong").text + "\n")
-    list_file.close()
-else:
-    for item in items:
-        tags = item.split("\t")
-        if len(tags) != 2:
-            login163.send_mail(login163.mailto_list, "error", "item length is not two.")
-        elif tags[0] != li_tag.find("a").get("href") or tags[1].strip("\n") !=  li_tag.find("strong").text:
-            login163.send_mail(["951004642@qq.com"], "new", "wu han li gong.")
-            list_file.close()
-            new_file = open(file_path, 'w')
-            for item in lis:
-                new_file.write(item.find("a").get("href") + "\t" + item.find("strong").text + "\n")
-            new_file.close()
-            print "send email"
-        else:
-            print "not to send email"
-        break
+def store_news_list(full_file, news_list):
+    with open(full_file, 'w') as f:
+        for news in news_list:
+            href = news.find('a').get('href')
+            date = news.find('strong').text
+            f.write(href + '\t' + date + '\n')
+
+
+try:
+    response = requests.get(whut_url).text
+    try:
+        soup = BeautifulSoup(response, "html5lib")
+        ul = soup.find('ul', attrs={"class":"normal_list2"})
+        news_list = ul.find_all('li')
+        first_href = news_list[0].find('a').get('href')
+        first_date = news_list[0].find('strong').text
+
+        first = False
+        if not os.path.exists(full_file):
+            store_news_list(full_file, news_list)
+            print "Status: Not need send email, no new item."
+            first = True
+
+        sended = False
+        with open(full_file) as f:
+            news_list_in_file = f.readlines()
+            for item in news_list_in_file:
+                items = item.split('\t')
+                if items[0] != first_href or items[1].strip('\n') != first_date:
+                    email163.send_mail(st_email, "New Item", whut_url + " have new items.")
+                    print "Status: sended email."
+                    sended = True
+                else:
+                    if not first:
+                        print "Status: Not need send email, no new item."
+                break
+
+        if sended:
+            store_news_list(full_file, news_list)
+
+    except Exception, e:
+        email163.send_mail(st_email, "Parse Error.", "Maybe your web page has changed, please check it.")
+        print "Parse Error:" + str(e)
+except Exception, e:
+    email163.send_mail(st_email, "Network Error.", "Can not get web page. Please check your network.")
+    print "Network Error:" + str(e)
